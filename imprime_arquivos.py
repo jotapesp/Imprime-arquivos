97262a7697b853045eb48d0e6e0050d46deb2ffa
joao <joaopedrodesanchespinheiro2@gmail.com>
2023-03-21 09:23:12 -0400

    Segundo bug, encontrado junto, argv tava como string

--- imprime_arquivos.py
+++ imprime_arquivos.py
@@ -4,6 +4,6 @@ if len(sys.argv) < 2:
     print("Usagem: imprime_arquivos.py [arquivo 1] [arquivo 2] ... [arquivo n]")
 
 for i in range(1, len(sys.argv)):
-    with open("sys.argv[i]", "r") as arquivo:
+    with open(sys.argv[i], "r") as arquivo:
         print(f" {sys.argv[i]} ".center(80, "="))
         print(arquivo.read())
