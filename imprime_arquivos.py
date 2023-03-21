bdcb63584d6d81a10586be01c8c3488dcaf1f0cb
joao <joaopedrodesanchespinheiro2@gmail.com>
2023-03-21 09:32:56 -0400

    Adicionado o tratamento p/ file not found

--- imprime_arquivos.py
+++ imprime_arquivos.py
@@ -3,7 +3,11 @@ import sys
 if len(sys.argv) < 2:
     print("Usagem: imprime_arquivos.py [arquivo 1] [arquivo 2] ... [arquivo n]")
 
-for i in range(1, len(sys.argv)):
-    with open(sys.argv[i], "r") as arquivo:
-        print(f" {sys.argv[i]} ".center(80, "="))
-        print(arquivo.read())
+try:
+    for i in range(1, len(sys.argv)):
+        with open(sys.argv[i], "r") as arquivo:
+            print(f" {sys.argv[i]} ".center(80, "="))
+            print(arquivo.read())
+except FileNotFoundError:
+    print("".center(80, "="))
+    print(f"Erro: arquivo {sys.argv[i]} não foi encontrado.")
