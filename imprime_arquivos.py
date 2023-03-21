c3adffcac0b8b3a35b55347219c0323eeb76b8f4
joao <joaopedrodesanchespinheiro2@gmail.com>
2023-03-21 09:20:18 -0400

    correção do primeiro bug: return estava sobrando e impedindo programa de rodar

--- imprime_arquivos.py
+++ imprime_arquivos.py
@@ -2,7 +2,7 @@ import sys
 
 if len(sys.argv) < 2:
     print("Usagem: imprime_arquivos.py [arquivo 1] [arquivo 2] ... [arquivo n]")
-    return 1
+
 for i in range(1, len(sys.argv)):
     with open("sys.argv[i]", "r") as arquivo:
         print(f" {sys.argv[i]} ".center(80, "="))
