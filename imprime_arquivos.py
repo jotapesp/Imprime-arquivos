4c74f7f57a22569554079724d0b746c28dc315d5
joao <joaopedrodesanchespinheiro2@gmail.com>
2023-03-21 09:49:25 -0400

    Adicionados tratamentos para caso de diretório

--- imprime_arquivos.py
+++ imprime_arquivos.py
@@ -1,4 +1,5 @@
 import sys
+import os.path
 
 if len(sys.argv) < 2:
     print("Usagem: imprime_arquivos.py [arquivo 1] [arquivo 2] ... [arquivo n]")
@@ -6,8 +7,16 @@ if len(sys.argv) < 2:
 try:
     for i in range(1, len(sys.argv)):
         with open(sys.argv[i], "r") as arquivo:
+            if os.path.isdir(sys.argv[i]):
+                raise IsADirectoryError
             print(f" {sys.argv[i]} ".center(80, "="))
             print(arquivo.read())
 except FileNotFoundError:
     print("".center(80, "="))
-    print(f"Erro: arquivo {sys.argv[i]} não foi encontrado.")
+    print(f"Erro: arquivo '{sys.argv[i]}' não foi encontrado.")
+except IsADirectoryError:
+    print("".center(80, "="))
+    print(f"Erro: '{sys.argv[i]}' é um diretório/pasta.")
+finally:
+    print("".center(80, "="))
+    print("Programa encerrado.")
